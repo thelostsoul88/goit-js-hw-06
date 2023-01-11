e7f54da9605d4a1b6b0961366d51d4a54a6e8bd2
Dmitriy <48189332+thelostsoul88@users.noreply.github.com>
2023-01-11 21:00:12 +0200

    secondTaskComplete

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const addUl = document.getElementById("ingredients");
+// -----------------------FOR-----------------------
+// const array = [];
+// for (const ingredient of ingredients) {
+//   const elemLi = document.createElement("li");
+//   elemLi.textContent = ingredient;
+//   elemLi.classList.add("item");
+//   array.push(elemLi);
+// }
+// addUl.append(...array);
+
+// -----------------------MAP--------------------
+const array = ingredients.map((ingredient) => {
+  const elemLi = document.createElement("li");
+  elemLi.textContent = ingredient;
+  elemLi.classList.add("item");
+  return elemLi;
+});
+addUl.append(...array);
