453aa0a0d256fefeafe559d0d777e171227de55d
Dmitriy <48189332+thelostsoul88@users.noreply.github.com>
2023-01-11 22:26:40 +0200

    third task complete

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,39 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+const addUl = document.querySelector(".gallery");
+// -----------------------------MAP-------------------------
+// const imgEl = images
+//   .map((img) => {
+//     return `<li class="gallery__item"><img class="gallery__img" src="${img.url}" alt="${img.alt}"></li>`;
+//   })
+//   .join("");
+// addUl.insertAdjacentHTML("beforeend", imgEl);
+// -----------------------------REDUCE----------------------
+const imgEl = images.reduce(
+  (acc, img) =>
+    (acc += `<li><img class="gallery__img" src="${img.url}" alt="${img.alt}"></li>`),
+  ""
+);
+addUl.insertAdjacentHTML("beforeend", imgEl);
+// -----------------------------FOR-------------------------
+// const liElem = document.createElement("li");
+// for (const img of images) {
+//   const imgElem = document.createElement("img");
+//   imgElem.src = img.url;
+//   imgElem.alt = img.alt;
+//   imgElem.classList.add("gallery__img");
+//   liElem.appendChild(imgElem);
+// }
+// addUl.appendChild(liElem);
