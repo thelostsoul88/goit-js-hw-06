8f4c6efcb01d0086baa41e3c6d98c1fe656935a8
Dmitriy <48189332+thelostsoul88@users.noreply.github.com>
2023-01-11 21:05:49 +0200

    second task complete

--- js/task-02.js
+++ js/task-02.js
@@ -9,20 +9,20 @@ const ingredients = [
 
 const addUl = document.getElementById("ingredients");
 // -----------------------FOR-----------------------
-// const array = [];
-// for (const ingredient of ingredients) {
-//   const elemLi = document.createElement("li");
-//   elemLi.textContent = ingredient;
-//   elemLi.classList.add("item");
-//   array.push(elemLi);
-// }
-// addUl.append(...array);
-
-// -----------------------MAP--------------------
-const array = ingredients.map((ingredient) => {
+const array = [];
+for (const ingredient of ingredients) {
   const elemLi = document.createElement("li");
   elemLi.textContent = ingredient;
   elemLi.classList.add("item");
-  return elemLi;
-});
+  array.push(elemLi);
+}
 addUl.append(...array);
+
+// -----------------------MAP--------------------
+// const array = ingredients.map((ingredient) => {
+//   const elemLi = document.createElement("li");
+//   elemLi.textContent = ingredient;
+//   elemLi.classList.add("item");
+//   return elemLi;
+// });
+// addUl.append(...array);
