3062da771ef6c4b6876d1ba49d4b3f10d2ca159c
Dmitriy <48189332+thelostsoul88@users.noreply.github.com>
2023-01-15 18:34:52 +0200

    ninth task complete

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorBtn = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+const body = document.querySelector("body");
+// --------------------- function in --------------
+colorBtn.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+});
+// --------------------- function out -------------
+// colorBtn.addEventListener("click", handleColor);
+// function handleColor() {
+//   const randomColors = getRandomHexColor();
+//   body.style.backgroundColor = randomColors;
+//   spanColor.textContent = randomColors;
+// }
