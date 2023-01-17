5e76766257a97430f831148ae272ad5d41657eb7
Dmitriy <48189332+thelostsoul88@users.noreply.github.com>
2023-01-17 19:08:46 +0200

    tenth task complete

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,30 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputEl = document.querySelector("input");
+const createEl = document.querySelector("[data-create]");
+const destroyEl = document.querySelector("[data-destroy]");
+const boxes = document.getElementById("boxes");
+
+function createBoxes(amount) {
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.cssText = `width: ${size}px;
+    height: ${size}px;
+    background-color: ${getRandomHexColor()};`;
+    size += 10;
+    boxes.appendChild(div);
+  }
+}
+
+createEl.addEventListener("click", () => {
+  const amount = inputEl.value;
+  createBoxes(amount);
+});
+
+destroyEl.addEventListener("click", () => {
+  boxes.innerHTML = "";
+});
